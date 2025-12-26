f289abf808de8d6d0af760bb568e1a0528c9486d
ET5521 <earth12345521@gmail.com>
2025-12-26 17:14:35 +0700

    lab10 commited

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<fstream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    float sum = 0.0, sum_of_square = 0.0;
+
+    string textline;
+    ifstream source("score.txt");
+
+    if(!source){
+        cout << "Error Cannot open file 'score.txt'" << endl;
+        return 1;
+    }
+
+    while(getline(source,textline)){
+        float value = stof(textline);
+        sum += value;
+        sum_of_square += value*value;
+        count++;
+    }
+
+    float mean = sum/count;
+    float dev = sqrt((sum_of_square/count) - (mean*mean));
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << dev << endl;
+}
